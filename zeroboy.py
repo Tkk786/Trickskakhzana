d3f6245ffde3b0c64ef6871dced07b1d2966d143
Tkk786 <65829515+Tkk786@users.noreply.github.com>
2020-05-24 06:42:08 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,16 +49,16 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•Tricks - ka -khzana------•◈•
+|__\~~) •||•Tricks - ka - khzana---------------•◈•
+|__(-----\  •◈•------Trickska-khzana--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92mTricks ka khzana
+\033[1;96mYoutube \033[1;93m: \033[1;FlowTricks ka khzana
+\033[1;96mFacebook  \033[1;93m: \033[1; Tricks ka khzana
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
-\033[1;91m======================================="""
+\033[1;96mAuthor  \033[1;93m: \033[1;92mTricks ka khzana
+\033[1;96mYoutube \033[1;93m: \033[1;92mTricks ka khzana
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Tricks ka khzana
+\033[1;96mGithub \033[1;93m: \033[1;92mgithub.com/Tkk786/Trickskakhzana
+\033[1;91m======================================"""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Tkk786"
+CorrectPassword = "Tkk786"
 
 loop = 'true'
 while (loop == 'true'):
